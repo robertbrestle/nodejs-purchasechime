cdc7e9746447a96606577bedf94f37e84c5edac7
Robert Brestle <68937744+robertbrestle@users.noreply.github.com>
2022-11-23 01:08:25 -0600

    added path.join

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 var fs = require("fs");
+var path = require("path");
 
 var bodyParser = require('body-parser');
 //var multer  = require('multer');
 
-app.disable('x-powered-by');
-app.use(express.static('public'));
+app.disable("x-powered-by");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(multer({ dest: '/tmp/'}));
 
 app.get('/', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
+   res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.get('/data', (req, res) => {
+app.get("/data", (req, res) => {
    res.header("Content-Type", "application/json");
    res.send(JSON.stringify({
       data: ["11/21/2022", "11:59:59 PM", 3, 432.10]
